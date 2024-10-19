70cff68c0c1a58923ee3de2176b4f25430752f53
neet-007 <moayedeffect@gmail.com>
2024-10-19 19:50:11 +0300

    added initail spliting of b+tree nodes and b+tree insertion

--- internal/bplustree/helpers.go
+++ internal/bplustree/helpers.go
@@ -142,6 +142,16 @@ func leafInsert(
 	nodeAppendRange(new, old, idx+1, idx, old.nkeys()-1)
 }
 
+func leafUpdate(
+	new BNode, old BNode, idx uint16,
+	key []byte, val []byte,
+) {
+	new.setHeader(BNODE_LEAF, old.nkeys())
+	nodeAppendRange(new, old, 0, 0, idx)
+	nodeAppedKV(new, idx, 0, key, val)
+	nodeAppendRange(new, old, idx+1, idx+1, old.nkeys()-idx-1)
+}
+
 func nodeAppedKV(new BNode, idx uint16, ptr uint64, key []byte, val []byte) {
 	new.setPtr(idx, ptr)
 
@@ -181,4 +191,95 @@ func nodeReplaceKidN(
 
 	nodeAppendRange(new, old, idx+inc, idx+1, old.nkeys()-(idx+1))
 
+}
+
+func findSplitIndex(old BNode) uint16 {
+	var nbytes uint16 = 0
+
+	for i := uint16(0); i < old.nkeys(); i++ {
+		pos := old.kvPos(i)
+
+		keyLen := binary.LittleEndian.Uint16(old[pos : pos+2])
+		valLen := binary.LittleEndian.Uint16(old[pos+2 : pos+4])
+
+		nbytes += 4 + keyLen + valLen
+		if nbytes > BTREE_PAGE_SIZE {
+			return i
+		}
+	}
+
+	return old.nkeys() / 2
+}
+
+// split a oversized node into 2 so that the 2nd node always fits on a page
+func nodeSplit2(left BNode, right BNode, old BNode) {
+	splitIdx := findSplitIndex(old)
+
+	nodeAppendRange(right, old, 0, 0, splitIdx)
+
+	nodeAppendRange(left, old, 0, splitIdx, old.nkeys()-splitIdx)
+
+	right.setHeader(BNODE_NODE, splitIdx)
+	left.setHeader(BNODE_NODE, old.nkeys()-splitIdx)
+}
+
+// split a node if it's too big. the results are 1~3 nodes.
+func nodeSplit3(old BNode) (uint16, [3]BNode) {
+	if old.nbytes() <= BTREE_PAGE_SIZE {
+		old = old[:BTREE_PAGE_SIZE]
+		return 1, [3]BNode{old}
+	}
+
+	left := BNode(make([]byte, 2*BTREE_PAGE_SIZE))
+	right := BNode(make([]byte, BTREE_PAGE_SIZE))
+
+	nodeSplit2(left, right, old)
+
+	if left.nbytes() <= BTREE_PAGE_SIZE {
+		left = left[:BTREE_PAGE_SIZE]
+		return 2, [3]BNode{left, right}
+	}
+
+	leftleft := BNode(make([]byte, 2*BTREE_PAGE_SIZE))
+	middle := BNode(make([]byte, BTREE_PAGE_SIZE))
+
+	nodeSplit2(leftleft, middle, left)
+	if leftleft.nbytes() > BTREE_PAGE_SIZE {
+		panic("spillting the node failed the num of bytes in leftleft should nevert be more than BTREE_PAGE_SIZE")
+	}
+
+	leftleft = leftleft[:BTREE_PAGE_SIZE]
+	return 3, [3]BNode{leftleft, middle, right}
+}
+
+func treeInsert(tree *BTree, node BNode, key []byte, val []byte) BNode {
+	new := BNode(make([]byte, 2*BTREE_PAGE_SIZE))
+
+	idx := nodeLookupLE(node, key)
+
+	switch node.btype() {
+	case BNODE_LEAF:
+		if bytes.Equal(key, node.getKey(idx)) {
+			leafUpdate(new, node, idx, key, val)
+		} else {
+			leafInsert(new, node, idx, key, val)
+		}
+	case BNODE_NODE:
+		nodeInsert(tree, new, node, idx, key, val)
+	default:
+		panic("bad node")
+	}
+
+	return new
+}
+
+func nodeInsert(tree *BTree, new BNode, node BNode, idx uint16, key []byte, val []byte) {
+	kptr := node.getPtr(idx)
+
+	knode := treeInsert(tree, tree.get(kptr), key, val)
+
+	nsplit, split := nodeSplit3(knode)
+	tree.del(kptr)
+
+	nodeReplaceKidN(tree, new, node, idx, split[:nsplit]...)
 }
