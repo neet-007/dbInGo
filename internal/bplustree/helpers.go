e980d6a636f6e9dca89c0d7fbc950ad5c8c3d8d2
neet-007 <moayedeffect@gmail.com>
2024-10-19 20:59:13 +0300

    added initail impleantaion of merging nodes

--- internal/bplustree/helpers.go
+++ internal/bplustree/helpers.go
@@ -327,3 +327,37 @@ func (tree *BTree) Insert(key []byte, val []byte) error {
 	}
 	return nil
 }
+
+func shouldMerge(
+	tree *BTree, node BNode, idx uint16, updated BNode,
+) (int, BNode) {
+	if updated.nbytes() > BTREE_PAGE_SIZE/4 {
+		return 0, BNode{}
+	}
+
+	if idx > 0 {
+		sibling := BNode(tree.get(node.getPtr(idx - 1)))
+		merged := sibling.nbytes() + updated.nbytes() - HEADER
+		if merged <= BTREE_PAGE_SIZE {
+			return -1, sibling
+		}
+	}
+	if idx+1 < node.nkeys() {
+		sibling := BNode(tree.get(node.getPtr(idx + 1)))
+		merged := sibling.nbytes() + updated.nbytes() - HEADER
+		if merged <= BTREE_PAGE_SIZE {
+			return +1, sibling
+		}
+	}
+
+	return 0, BNode{}
+}
+
+// remove a key from a leaf node
+func leafDelete(new BNode, old BNode, idx uint16)
+
+// merge 2 nodes into 1
+func nodeMerge(new BNode, left BNode, right BNode)
+
+// replace 2 adjacent links with 1
+func nodeReplace2Kid(new BNode, old BNode, idx uint16, ptr uint64, key []byte)
